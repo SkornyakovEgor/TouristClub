26acbe43ac6419e0c359baf83685cccde18085c2
Егор <58623077+SkornyakovEgor@users.noreply.github.com>
2021-05-18 01:28:17 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -51,25 +51,3 @@ function showTabs(){
 }
 
 /*------------------------------------------------------------------------------------------------------*/
-
-function initMap() {
-    var coordinates = {lat: 47.212325, lng: 38.933663},
-    
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: coordinates
-        });
-}
-
-function initMap() {
-    var coordinates = {lat: 47.212325, lng: 38.933663},
-
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: coordinates
-        }),
-    
-        marker = new google.maps.Marker({
-            position: coordinates,
-            map: map
-        });
-}
-
