610f9e633bcd8cb250b074f76071a79a9d594507
Егор <58623077+SkornyakovEgor@users.noreply.github.com>
2021-05-26 01:13:05 +0500

    v2.6

--- script/script.js
+++ script/script.js
@@ -27,31 +27,6 @@ function closeModal(){
 
 /*------------------------------------------------------------------------------------------------------*/
 
-function init(e){
-    let tabBody = document.querySelectorAll('.activity-content');
-    let tabKey = document.querySelectorAll('.tab');
-    for(let i = (e); i < tabBody.length; i++){
-        tabBody[i].style.display = "none";
-        tabKey[i].classList.remove("active");
-    }
-}
-init(1); 
-
-let tab = document.querySelectorAll('.tab');
-tab.forEach(function(element){
-    element.onclick = showTabs;
-});
-
-function showTabs(){
-    let data = this.getAttribute('data');
-    console.log(data);
-    init(0); 
-    document.querySelector(`.activity-content[data ="${data}"]`).style.display = 'block';
-    document.querySelector(`.tab[data ="${data}"]`).classList.add("active");
-}
-
-/*------------------------------------------------------------------------------------------------------*/
-
 let mas = ["../TouristClub/img/gallery/1.jpg",
            "../TouristClub/img/gallery/2.jpg",
            "../TouristClub/img/gallery/3.jpg",
@@ -80,11 +55,9 @@ function left_arrow(){
 document.querySelector("#icon").onclick = function(){
     document.querySelector("#navBlock").style.marginTop = "0px";
 }
-
 document.querySelector("#closeMenuButton").onclick = function(){
     document.querySelector("#navBlock").style.marginTop = "-1000px";
 }
-
 
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function(){
@@ -93,8 +66,66 @@ window.onscroll = function(){
         document.getElementById("navBlock").style.top = "0";
         document.getElementById("icon").style.top = "0";
     } else {
-        document.getElementById("navBlock").style.top = "-100px";
-        document.getElementById("icon").style.top = "-100px";
+        document.getElementById("navBlock").style.top = "-150px";
+        document.getElementById("icon").style.top = "-150px";
     }
     prevScrollpos = currentScrollPos;
 }
+/*------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------*/
+
+function init(e){
+    let tabBody = document.querySelectorAll('.activity-content');
+    let tabKey = document.querySelectorAll('.dot');
+    for(let i = (e); i < tabBody.length; i++){
+        tabBody[i].style.display = "none";
+        tabKey[i].classList.remove("active");
+
+    }
+}
+init(1); 
+
+let tab = document.querySelectorAll('.dot');
+tab.forEach(function(element){
+    element.onclick = showTabs;
+});
+
+function showTabs(){
+    let data = this.getAttribute('data');
+    console.log(data);
+    init(0); 
+    document.querySelector(`.activity-content[data ="${data}"]`).style.display = 'block';
+    document.querySelector(`.dot[data ="${data}"]`).classList.add("active");
+    makeTimer();
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("activity-content");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+let timer = 0;
+makeTimer(); //Создаем интервал 
+function makeTimer(){
+   clearInterval(timer) //Очистим интервал, это позволит прервать его работу и отменить перелистывание
+   timer = setInterval(function(){
+        showSlides(slideIndex);
+    },10000);
+ } 
